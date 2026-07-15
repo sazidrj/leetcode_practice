e7854ff303541e739055228e147bc6a888ac5907
Sazid Ali <41997284+sazidrj@users.noreply.github.com>
2026-07-15 23:52:52 +0530

    LeetCode: Solved Number of Islands [C++]

--- LeetCode/Array/Number of Islands/solution.cpp
+++ LeetCode/Array/Number of Islands/solution.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
- void eraseSurrounded1(int i, int j, vector<vector<char>> &grid){
-	if(i < 0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j] == '0')
-		return;
 
-	grid[i][j] = '0';
+    void eraseSurrounded1(int i, int j, int n, int m, vector<vector<char>> &grid){
+        if(i < 0 || i >= n || j < 0 || j >= m || grid[i][j] == '0'){
+            return ;
+        }
 
-	eraseSurrounded1(i,j+1, grid);
-	eraseSurrounded1(i-1,j, grid);
-	eraseSurrounded1(i,j-1,grid);
-	eraseSurrounded1(i+1, j, grid);
-   
-   return;
-}
+        grid[i][j] = '0';
+        eraseSurrounded1(i+1, j, n, m, grid);
+        eraseSurrounded1(i-1, j, n, m, grid);
+        eraseSurrounded1(i, j-1, n, m, grid);
+        eraseSurrounded1(i, j+1, n, m, grid);
 
-
-int numIslands(vector<vector<char>>& grid) {
-   int n = grid.size();
-   int m = grid[0].size();
-   int count = 0;
-
-   for(int i = 0; i<n; i++){
-   	  for(int j = 0; j<m; j++){
-          if(grid[i][j] == '1'){
-              count++;
-              eraseSurrounded1(i, j, grid);
-          }
-   	  }
-   }
-   return count;
-}
+        return;
+    }
 
 
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int count = 0;
 
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(grid[i][j] == '1'){
+                    count += 1;
+                    eraseSurrounded1(i, j, n, m ,grid);
+                }
+            }
+        }
+
+        return count;
+
+    }
 };

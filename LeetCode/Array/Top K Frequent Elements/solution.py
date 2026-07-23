7224cdef5e904db5a089ce7d7aae0f08efe3f591
Sazid Ali <41997284+sazidrj@users.noreply.github.com>
2026-07-23 22:51:31 +0530

    LeetCode: Solved Top K Frequent Elements [Python3]

--- LeetCode/Array/Top K Frequent Elements/solution.py
+++ LeetCode/Array/Top K Frequent Elements/solution.py
@@ -1,5 +1,13 @@
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        return [num for num, count in Counter(nums).most_common(k)]
+        count_map = Counter(nums)
+
+        pq = []
+
+        for num, count in count_map.items():
+            heapq.heappush(pq, (count, num))
+            if len(pq) > k:
+                heapq.heappop(pq)
+
+        return [num for count, num in pq]
         

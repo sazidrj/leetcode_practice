7b9804ba4b2b87dad51c1dc2fd713229c7625322
Sazid Ali <41997284+sazidrj@users.noreply.github.com>
2026-07-23 23:08:56 +0530

    LeetCode: Solved Top K Frequent Elements [C++]

--- LeetCode/Array/Top K Frequent Elements/solution.cpp
+++ LeetCode/Array/Top K Frequent Elements/solution.cpp
@@ -1,32 +1,54 @@
 class Solution {
+private: 
+    vector<int> unique;
+    map<int,int> count_map;
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        if(k == nums.size()){
-            return nums;
-        }
 
-        unordered_map<int, int> count_map;
-        for(int n: nums){
-            count_map[n] += 1;
-        }
+    int partition(int left, int right, int pivot_index){
+        int pivot_frequency = count_map[unique[pivot_index]];
+        swap(unique[pivot_index], unique[right]);
 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-
-        for(auto [num, count]: count_map){
-            pq.push({count, num});
-
-            if(pq.size() > k){
-                pq.pop();
+        int store_index = left;
+        for(int i = left; i <= right; i++){
+            if(count_map[unique[i]] < pivot_frequency){
+                swap(unique[store_index], unique[i]);
+                store_index += 1;
             }
-        } 
-
-        vector<int> res;
-
-        while(!pq.empty()){
-            res.push_back(pq.top().second);
-            pq.pop();
         }
 
-        return res;
+        swap(unique[right], unique[store_index]);
+
+        return store_index;
+    }
+
+    void quickselect(int left, int right, int k_smallest){
+        if(left == right) return;
+
+        int pivot_index = left + rand() % (right - left + 1);
+
+        pivot_index = partition(left, right, pivot_index);
+
+        if(k_smallest == pivot_index){
+            return;
+        }else if(k_smallest < pivot_index){
+            quickselect(left, pivot_index - 1, k_smallest);
+        }else{
+            quickselect(pivot_index+1, right, k_smallest);
+        }
+    }
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+       for(int n: nums){
+        count_map[n] += 1;
+       }
+
+       int n = count_map.size();
+       for(pair<int,int> p: count_map){
+        unique.push_back(p.first);
+       }
+
+       quickselect(0, n-1, n-k);
+       vector<int> top_k_frequent(k);
+       copy(unique.begin() + n-k, unique.end(), top_k_frequent.begin());
+       return top_k_frequent;
     }
 };

23173a0c966104a435f186e5c748ce43010dc824
Sazid Ali <41997284+sazidrj@users.noreply.github.com>
2026-07-27 23:53:09 +0530

    LeetCode: Solved Course Schedule [Java]

---
+++ LeetCode/Depth-First Search/Course Schedule/solution.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> g = new ArrayList();
+        for(int i = 0; i<numCourses; i++){
+            g.add(new ArrayList<>());
+        }
+        int[] indegree = new int[numCourses];
+
+        for(int i = 0; i<prerequisites.length; i++){
+            int a = prerequisites[i][0];
+            int b = prerequisites[i][1];
+            g.get(b).add(a);
+            indegree[a]++;
+        }
+
+        Queue<Integer> q = new LinkedList();
+        List<Integer> v = new ArrayList();
+
+        for(int i = 0; i<numCourses; i++){
+            if(indegree[i] == 0){
+                q.offer(i);
+            }
+        }
+
+        while(!q.isEmpty()){
+            int f = q.poll();
+            v.add(f);
+            for(int neighbor : g.get(f)){
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.offer(neighbor);
+                }
+            }
+        }
+
+        if(v.size() == numCourses)
+            return true;
+        else
+            return false;
+    
+    }
+}
